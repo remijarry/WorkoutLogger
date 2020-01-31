35af3e74ef9c1b82aa0b619d4ef2a525e9e5f4b9
remijarry <rem_ay@hotmail.fr>
2020-01-31 16:16:51 +1300

    Create View Models

--- src/WorkoutLogger.Web/Controllers/BarbellController.cs
+++ src/WorkoutLogger.Web/Controllers/BarbellController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkoutLogger.Web.ViewModels;
 
 namespace WorkoutLogger.Web.Controllers
 {
@@ -19,7 +20,52 @@ namespace WorkoutLogger.Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var dummyData = new List<CategoryViewModel>
+            {
+                new CategoryViewModel()
+                {
+                    Id = 1,
+                    Name = "Squats",
+                    Exercises = new List<ExerciseViewModel>()
+                    {
+                        new ExerciseViewModel()
+                        {
+                            Id = 1,
+                            Name = "Back Squat",
+                            OneRepMax = new RepMaxViewModel{Date = new DateTime(), Weight = 128 },
+                        },
+                        new ExerciseViewModel()
+                        {
+                            Id = 2,
+                            Name = "Front Squat",
+                            OneRepMax = new RepMaxViewModel{Date = new DateTime(2019,7,12), Weight = 105 },
+                        }
+                    }
+
+                },
+                new CategoryViewModel()
+                {
+                    Id = 2,
+                    Name = "Cleans",
+                    Exercises = new List<ExerciseViewModel>()
+                    {
+                        new ExerciseViewModel()
+                        {
+                            Id = 3,
+                            Name = "Power Clean",
+                            OneRepMax = new RepMaxViewModel{Date = new DateTime(), Weight = 100 },
+                        },
+                        new ExerciseViewModel()
+                        {
+                            Id = 4,
+                            Name = "Squat Clean",
+                            OneRepMax = new RepMaxViewModel{Date = new DateTime(2019,7,12), Weight = 105 },
+                        }
+                    }
+
+                }
+            };
+            return View(dummyData);
         }
     }
 }
---
+++ src/WorkoutLogger.Web/ViewModels/ExerciseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutLogger.Web.ViewModels
+{
+    public class ExerciseViewModel : BaseViewModel
+    {
+        public string Name { get; set; }
+
+        public RepMaxViewModel OneRepMax { get; set; }
+
+        public RepMaxViewModel TwoRepMax { get; set; }
+
+        public RepMaxViewModel ThreeRepMax { get; set; }
+
+        public RepMaxViewModel FiveRepMax { get; set; }
+    }
+}
